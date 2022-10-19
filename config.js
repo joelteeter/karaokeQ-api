003c9504076dfb8acaa1729d542ff16f400fe270
joelteeter <joelteeter@gmail.com>
2022-10-18 20:47:39 -0600

    Update config.js

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const config = {
 	db: {
 		//normally don't do this...
-		host: "127.0.0.1",
-		user: "karaokeq",
-		password: "-.wmX_ytwL2iLuC",
-		database: "karaokeq",
-		port: 3307
+		host: process.env.DB_HOSTNAME,
+		user: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
+		database: process.env.DB_DATABASE,
+		port: process.env.DB_PORT || 3306
 	},
 	listPerPage: 10,
 };
